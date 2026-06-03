b0257bd9bcba5764991ca7950a3230e3fea48d00
Vero2518 <vmaheshnandva1@meta.com>
2026-06-03 15:01:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const verifyToken = process.env.vibecode;
 
 // Route for GET requests
 app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verifyToken': token } = req.query;
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 // Route for POST requests
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(\n\nWebhook received ${timestamp}\n);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(\nListening on port ${port}\n);
+  console.log(`\nListening on port ${port}\n`);
 });
